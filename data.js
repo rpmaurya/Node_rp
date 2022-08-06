4377f5c3f79c5aa1fa4d500d68c9ea48b950ea70
rpmaurya <ramkewal143@gmail.com>
2022-08-06 01:53:54 -0400

    data

---
+++ data.js
@@ -0,0 +1,7 @@
+const data =[
+    {name:'rpmaurya',email:'ramkewal143@gmail.com',contect:'9919572134'},
+    {name:'mohan',email:'mohal@gmail.com',contect:'9919572135'},
+    {name:'anuradha',email:'anu@gmail.com',contect:'9919572154'}
+
+]
+module.exports= data;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello bro welcome my page");
+const http =require('http');
+const data =require('./data');
+http.createServer((req,resp)=>{
+resp.writeHead(200,{'content-type':'application\json'});
+resp.write(JSON.stringify(data));
+resp.end();
+}).listen(5000);
