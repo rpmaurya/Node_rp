79e8232412828d3a847ee5d8bedb2fc16ccb76b0
Madhu <Madhu@example.com>
2022-08-06 14:12:22 +0530

    minor changes

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
-const data =[
-    {name:'rpmaurya',email:'ramkewal143@gmail.com',contect:'9919572134'},
-    {name:'mohan',email:'mohal@gmail.com',contect:'9919572135'},
-    {name:'anuradha',email:'anu@gmail.com',contect:'9919572154'}
+const data = [
+    { name: 'rpmaurya1', email: 'ramkewal143@gmail.com', contect: '9919572134' },
+    { name: 'mohan', email: 'mohal@gmail.com', contect: '9919572135' },
+    { name: 'anuradha', email: 'anu@gmail.com', contect: '9919572154' }
 
 ]
-module.exports= data;
+module.exports = data;
