455334dc6462cc592ea4d261f44524827f413dc9
Madhu <Madhu@example.com>
2022-08-08 09:58:29 +0530

    pnf page create

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const publicpath = path.join(__dirname, 'public');
+app.get('', (req, resp) => {
+    resp.sendFile(`${publicpath}/index.html`)
+}),
+    app.get('/about', (req, resp) => {
+        resp.sendFile(`${publicpath}/about.html`)
+    }),
+    app.get('/help', (req, resp) => {
+        resp.sendFile(`${publicpath}/help.html`)
+    }),
+    app.get('*', (req, resp) => {
+        resp.sendFile(`${publicpath}/pnf.html`)
+    })
 
-app.use(express.static(publicpath));
+//app.use(express.static(publicpath));
 app.listen(5000);
